086cdd3fd0dafe60f5538477fbc3fa844978850d
lailesh1999 <laileshveigas08@gmail.com>
2026-01-29 12:26:35 +0530

    Created UserModel

---
+++ backend/src/models/User.ts
@@ -0,0 +1,21 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database";
+
+class User extends Model {}
+
+User.init(
+  {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true }
+  },
+  {
+    sequelize,
+    tableName: "users",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: false
+  }
+);
+
+export default User;
